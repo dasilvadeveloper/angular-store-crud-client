e367a13e7a0ecd5cbc8135e5edbbbce1c350ce37
Hendrik Lelis <dasilvadeveloper@icloud.com>
2022-06-22 08:52:12 +0100

    Stuctural directive creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,9 @@ import { HomeComponent } from './views/home/home.component'
 
 import { MatCardModule } from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { RedDirective } from './directives/red.directive'
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { RedDirective } from './directives/red.directive'
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
